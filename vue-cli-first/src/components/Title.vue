b6f608af15003bb519eaf3855deb75c90a0ac4f5
Luca Giozza <luca.giozza@gmail.com>
2021-07-05 15:19:40 +0200

    esercizio vue-cli

---
+++ vue-cli-first/src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- qui metto il mio h1 dell'esercizio -->
+    <h1> La mia prima app con Vue CLI!</h1>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 25px;
+  color:blue
+}
+</style>
