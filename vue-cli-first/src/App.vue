f44fca42858601f2d93272856e769dcf4137d79d
Luca Giozza <luca.giozza@gmail.com>
2021-07-05 15:58:34 +0200

    esercizio con vue cli

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
      <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>  -->
-    <title/>
+    <Title/>
   </div>
 </template>
 
@@ -12,9 +12,8 @@
 
 export default {
   name: 'App',
-  components: {
-    
-     Title
+  components : {
+    Title
   }
 }
 </script>
